1bc91b3de96b8069b3e428a426cbdacf492ec8b5
EriozDev <cilochemylan83@gmail.com>
2024-06-10 17:06:02 +0200

    Init Objec

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'bodacious'
+games { 'gta5' }
+description '__RENDER PROJECT'
+author 'CFX SHOP'
+
+lua54 'yes'
+
+client_script {
+	'client/*.lua'
+}
+
+            
+server_scripts {
+'@mysql-async/lib/MySQL.lua',
+'server/*.lua',
+}
