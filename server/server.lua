0ca9b1e8b16df2532c2ac37d66d4f711de4d6d5e
EriozDev <cilochemylan83@gmail.com>
2024-06-14 12:54:45 +0200

    Update funcs

--- server/server.lua
+++ server/server.lua
@@ -1,2 +1,10 @@
 Utils = {}
 Utils.Player = {}
+
+function Utils.Player.getPlayerName(player)
+    return GetPlayerName(player)
+end
+
+function Utils.Player.getPlayerPing(player)
+    return GetPlayerPing(player)
+end
