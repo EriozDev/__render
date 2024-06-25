98ccca04485ae471b7e0dcf518f527ef82434b2c
EriozDev <cilochemylan83@gmail.com>
2024-06-25 22:39:13 +0200

    Add debug mod

--- server/server/main.lua
+++ server/server/main.lua
@@ -1,1 +1,23 @@
+FrameWork.db = {}
+
 DEV = CONFIG.DEVMOD
+
+function FrameWork.db.getAllLicenses(callback)
+    MySQL.Async.fetchAll('SELECT license FROM render_accounts', {}, function(result)
+        local licenses = {}
+        for i=1, #result, 1 do
+            table.insert(licenses, result[i].license)
+        end
+        if callback then
+            callback(licenses)
+        end
+    end)
+end
+
+if DEV then
+    FrameWork.db.getAllLicenses(function(licenses)
+        for _, license in ipairs(licenses) do
+            print('(^6DEBUG^0) => (^3Player^0) => Loaded => ', license)
+        end
+    end)
+end
